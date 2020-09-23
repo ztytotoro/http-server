e7f0a005d9a7028f1ec4b23f4766246f5237c832
Yarn <ztytotoro@users.noreply.github.com>
2020-09-24 00:05:20 +0800

    init project

---
+++ types.ts
@@ -0,0 +1,17 @@
+export const MEDIA_TYPES: Record<string, string> = {
+  '.md': 'text/markdown',
+  '.html': 'text/html',
+  '.htm': 'text/html',
+  '.json': 'application/json',
+  '.map': 'application/json',
+  '.txt': 'text/plain',
+  '.ts': 'text/typescript',
+  '.tsx': 'text/tsx',
+  '.js': 'application/javascript',
+  '.jsx': 'text/jsx',
+  '.gz': 'application/gzip',
+  '.css': 'text/css',
+  '.wasm': 'application/wasm',
+  '.mjs': 'application/javascript',
+  '.mp4': 'video/mp4',
+};
